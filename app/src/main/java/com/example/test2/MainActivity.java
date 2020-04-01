cdd363ef0eb14e26afc40fd8686a9985ed7fb951
max <maxime.joubet>
2020-04-01 17:28:54 +0200

    01/04

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.test2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,15 +11,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Button valider;
-    EditText debut;
+    TextView debut;
     Spinner choix;
-    EditText record;
-    public int best = 0;
+    TextView record;
+    int nbrecord = 0;
     String text;
 
     @Override
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         debut = findViewById(R.id.debut); // variable qui affiche la partie va commencer
         record = findViewById(R.id.record);
-        setRecord(best); // affiche le record actuel
+        setRecord(); // affiche le record actuel
         choix = (Spinner) findViewById(R.id.choix); // pour choisir la langue
         ArrayAdapter<CharSequence> monAdapter = ArrayAdapter.createFromResource(this, R.array.tab_choix, android.R.layout.simple_spinner_item);
         monAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -41,10 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // on met ici ce qu’il faut faire lorsqu’aucun item n’est selectionne
-                System.out.println("aucun item n’est selectionné");
             }
-
         });
 
         valider = (Button) findViewById(R.id.valider); // bouton pour lancer la partie
@@ -56,25 +55,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
     private void visualiser_la_suite() {
         String test=text;
-        if (test == "Francais") {
+        if (test.equals("Francais")){
             Intent intent = new Intent(this, francais.class);
             startActivity(intent);
+
             System.out.println(text);
 
         }
-        if (test == "Anglais") {
+        if (test.equals("Anglais")) {
             Intent intent = new Intent(this, anglais.class);
             startActivity(intent);
-        } if(text != "Francais" && text != "Anglais") debut.setText("Selectionnez un item avant de commencer");
-    }
+        }    }
 
-    public void setRecord(int best) {
-        record.setText("Votre record actuel est : " + best);
+    public void setRecord() {
+        SharedPreferences sp = getSharedPreferences("your_prefs", anglais.MODE_PRIVATE);
+        nbrecord = sp.getInt("nbrecord", -1);
+        record.setText("Votre record actuel est : " + nbrecord);
     }
 
 }
--- app/src/main/java/com/example/test2/anglais.java
+++ app/src/main/java/com/example/test2/anglais.java
@@ -2,13 +2,111 @@ package com.example.test2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class anglais extends AppCompatActivity {
+    TextView titreAng;
+    Button commencer;
+    TextView timer;
+    TextView record;
+    TextView score;
+    CountDownTimer countdown;
+    long tempsrestant = 120000; // 2 min
+    TextView question;
+    Button reponse1,reponse2,reponse3,reponse4;
+    int nbrecord =0;
+    int nbscore=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anglais);
+        reponse1=findViewById(R.id.reponse1);
+        reponse2=findViewById(R.id.reponse2);
+        reponse3=findViewById(R.id.reponse3);
+        reponse4=findViewById(R.id.reponse4);
+        question=findViewById(R.id.question);
+        record=findViewById(R.id.record);
+        score=findViewById(R.id.score);
+        timer=findViewById(R.id.timer);
+        commencer=findViewById(R.id.commencer);
+        titreAng=findViewById(R.id.titreAng);
+
+        reponse1.setVisibility(View.INVISIBLE);
+        reponse2.setVisibility(View.INVISIBLE);
+        reponse3.setVisibility(View.INVISIBLE);
+        reponse4.setVisibility(View.INVISIBLE);
+        question.setVisibility(View.INVISIBLE);
+        setRecord();
+        commencer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+                afficherQuestion();
+            }
+        });
+
+    }
+void reponse (){
+        countdown.cancel();
+        setScore();
+        setRecord();
+
+}
+void startTimer(){
+    countdown = new CountDownTimer(tempsrestant, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            tempsrestant=millisUntilFinished;
+            updateTimer();
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
+    }.start();
+    commencer.setText("PAUSE");
+}
+void setScore (){
+        nbscore += (int) (10-tempsrestant);
+        score.setText("Score actuel : "+ nbscore);
+}
+
+void updateTimer(){
+    int minutes = (int) tempsrestant/60000;
+    int secondes = (int) tempsrestant % 60000/1000;
+    String tempsRestant;
+    tempsRestant = "" + minutes;
+    tempsRestant += ":";
+    if (secondes < 10) tempsRestant += "0";
+    tempsRestant += secondes;
+    timer.setText(tempsRestant);
+}
+void afficherQuestion (){
+    commencer.setVisibility(View.INVISIBLE);
+    titreAng.setVisibility(View.INVISIBLE);
+    reponse1.setVisibility(View.VISIBLE);
+    reponse2.setVisibility(View.VISIBLE);
+    reponse3.setVisibility(View.VISIBLE);
+    reponse4.setVisibility(View.VISIBLE);
+    question.setVisibility(View.VISIBLE);
+}
+public void setRecord() {
+    SharedPreferences sp = getSharedPreferences("your_prefs", anglais.MODE_PRIVATE);
+    nbrecord = sp.getInt("nbrecord", -1);
+    if (nbscore>nbrecord) {
+        nbrecord=nbscore;
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("nbrecord", nbrecord);
+        editor.commit();}
+    record.setText("Votre record actuel est : " + nbrecord);
     }
 }
